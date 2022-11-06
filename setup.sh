17db2c6fb0e28f949cd6b594507945527fd3d062
jesuscumpli <jesuscumpli@uma.es>
2022-11-06 22:18:08 +0100

    init

--- setup.sh
+++ setup.sh
@@ -1,14 +1,14 @@
-sudo cp . /opt/controlSystem
-sudo cd /opt/controlSystem
+sudo cp . /opt/device
+sudo cd /opt/device
 
 sudo setfacl -R -m u:control:rwx myfolder # Change username control for the specific User of the machine
 
 sudo apt install python3-pip
-sudo pip3 install -r /opt/controlSystem/requirements.txt
+sudo pip3 install -r /opt/device/requirements.txt
 
-sudo cp /opt/controlSystem/services/send_sensible_data/send_sensible_data.service /etc/systemd/system/send_sensible_data.service
-sudo cp /opt/controlSystem/services/receive_operations/receive_operations.service /etc/systemd/system/receive_operations.service
-sudo cp /opt/controlSystem/services/receive_operations/receive_operations.socket /etc/systemd/system/receive_operations.socket
+sudo cp /opt/device/services/send_sensible_data/send_sensible_data.service /etc/systemd/system/send_sensible_data.service
+sudo cp /opt/device/services/receive_operations/receive_operations.service /etc/systemd/system/receive_operations.service
+sudo cp /opt/device/services/receive_operations/receive_operations.socket /etc/systemd/system/receive_operations.socket
 
 sudo systemctl daemon-reload
 sudo systemctl enable send_sensible_data.service
