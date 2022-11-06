b205c6234adec7d75fdd5f0679d2aa719e1b97cf
jesuscumpli <jesuscumpli@uma.es>
2022-11-06 17:25:17 +0100

    init

--- services/receive_operations/receive_operations.py
+++ services/receive_operations/receive_operations.py
@@ -15,27 +15,46 @@ class Handler(StreamRequestHandler):
         try:
             self.data = self.rfile.readline().strip()
             logging.info("DATOS RECIBIDOS DE <%s>: %s" % (self.client_address, self.data))
-            self.normalize_data()
-            logging.info("Operacion recibida: " + str(self.data))
-            # TODO
+            self.get_config_data()
+            self.decode_message_info()
+            self.verify_IP_control_system()
+            self.change_configuration_by_operation()
+            logging.info("Operacion REALIZADA: " + str(self.data))
 
         except Exception as e:
             logging.exception(str(e))
 
+    def get_config_data(self):
+        with open("/opt/device/config/config.json") as f:
+            self.config_data = json.load(f)
+
+    def decode_message_info(self):
+        message = self.data
+        message = message.decode("ISO-8859-1")
+        message = json.loads(message)
+        self.message = message
+
+    def verify_IP_control_system(self):
+        IP_received = self.message["ip_control_system"]
+        IP_control_system = self.config_data["ip_control_system"]
+        if str(IP_received) != str(IP_control_system):
+            raise Exception("IP RECEIVED IS NOT CONTROL SYSTEM: " + str(IP_received))
+
     def normalize_data(self):
-        data = self.data
-        try:
-            data = data.decode()
-        except:
-            try:
-                data = data.decode('ISO-8859-1')
-            except:
-                data = str(data)
+        data = self.message["data"]
         try:
             data = json.loads(data)
         except Exception as e:
             logging.exception(str(e))
-        self.data = json.dumps(data)
+        self.operation = data
+
+    def change_configuration_by_operation(self):
+        key_config = self.operation["key_config"]
+        value_config = self.operation["value_config"]
+        config_data = self.config_data
+        config_data[key_config] = value_config
+        with open("/opt/device/config/config.json", 'w') as config_file:
+            json.dump(config_data, config_file)
 
 class Server(TCPServer):
     SYSTEMD_FIRST_SOCKET_FD = 3
--- services/send_sensible_data/send_sensible_data.py
+++ services/send_sensible_data/send_sensible_data.py
@@ -8,29 +8,32 @@ from encryption.functions import *
 from encryption.encryption import *
 from encryption.config import *
 import json
-import random
-import os
-import time
 from utils.generate_random_data import *
-import socket, time, sys
-import threading
+import socket, time
 
 MIN_SLEEP = 60
 MAX_SLEEP = 60 * 3
 
-f = open("/opt/device/config/config.json")
-config_data = json.load(f)
+config_data = None
 
-
-def send_data(message):
+def send_data(data):
     sensible_data = config_data["sensible_data"]
     ip_to_send = config_data["ip_control_system"]
     port_to_send = config_data["port_normal_listen_service"]
+    my_ip = config_data["IP_data"]
+
     if sensible_data:
-        message = encrypt_message(message)
+        data = encrypt_message(data)
         port_to_send = config_data["port_sensible_listen_service"]
     else:
-        message = message.encode()
+        data = data.encode("ISO-8859-1")
+
+    private_key = load_private_key("/opt/device/encryption/PRIVATE_KEY")
+    signature = firm_data(data, private_key)
+    message = {"IP_data": my_ip, "data": data.decode("ISO-8859-1"), "signature": signature.decode("ISO-8859-1")}
+    message = json.dumps(message)
+    message = message.encode("ISO-8859-1")
+
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((ip_to_send, port_to_send))
         s.sendall(message)
@@ -46,11 +49,14 @@ def encrypt_message(message_decrypted):
 
 
 if __name__ == "__main__":
+    global config_data
     logging.info("Initializing send sensible data service...")
 
     while True:
         try:
             logging.info("Generating new sample data...")
+            with open("/opt/device/config/config.json") as f:
+                config_data = json.load(f)
             data_to_send = generate_random_data_by_type_device(config_data["type_device"])
             data_to_send = json.dumps(data_to_send)
             logging.info("DATA TO SEND: " + str(data_to_send))
